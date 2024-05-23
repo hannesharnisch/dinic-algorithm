5afce06dce55029248cc4393395c516439359a7e
Hannes Harnisch <h.harnisch@fu-berlin.de>
2024-05-23 21:34:51 +0200

    feat: updated save network flow to print the network

--- lib/helper.py
+++ lib/helper.py
@@ -1,2 +1,6 @@
-def save_network_flow(network_flow):
-    pass
+from lib.solver.solverResult import SolverResult
+
+
+def save_network_flow(network_flow: SolverResult):
+    print("Saving network flow to file")
+    print(network_flow)
