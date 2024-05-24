90f04f7ef747d3e2b88399235b8c7dec9a4947ee
Hannes Harnisch <h.harnisch@fu-berlin.de>
2024-05-24 17:14:58 +0200

    feat: added demo network and network loading

--- lib/networkInput.py
+++ lib/networkInput.py
@@ -1,5 +1,5 @@
 import json
-from lib.network.capacitatedArc import CapacitatedArc
+from lib.network.capacitatedArc import CapacitatedArc, Capacity
 from lib.network.node import Node
 
 
@@ -10,10 +10,13 @@ class NetworkInput:
     def load_data_from_txt_file(self, data_path_and_name: str) :
         with open(data_path_and_name, 'r') as file:
             data = json.load(file)
-            for node in data['nodes']:
-                self.nodes.append(node)
+            for node, value in data['nodes'].items():
+                print(node)
+                self.nodes.append(Node(node, value['demand']))
             for arc in data['arcs']:
-                self.arcs.append(arc)
+                print(arc)
+                capacity = Capacity(ub=arc['upper_bound'], lb=arc['lower_bound'], current=arc['lower_bound'])
+                self.arcs.append(CapacitatedArc(arc['from'], arc['to'], capacity))
 
 
 
