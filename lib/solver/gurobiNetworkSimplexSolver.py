5c96c6a1203ab38fa4299a2612ecec802c78fcca
Hannes Harnisch <h.harnisch@fu-berlin.de>
2024-05-30 13:59:17 +0200

    fix: wrong import

--- lib/solver/gurobiNetworkSimplexSolver.py
+++ lib/solver/gurobiNetworkSimplexSolver.py
@@ -1,13 +1,13 @@
 
 
 from lib.solver.solver import Solver
-from lib.solver.solverResult import SolverResult
+from lib.solver.solverState import SolverState
 
 
 class GurobiNetworkSimplexSolver(Solver):
 
-    def solve(self, network) -> SolverResult:
+    def solve(self, network) -> SolverState:
         #initial_result = self.initial_result_solver.solve(network)
         #TODO: implement gurobi network simplex
-        return SolverResult(network=network)
+        return SolverState(network=network)
         
