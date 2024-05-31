520391ad529ca4029ce71b1afb337ac4340ada2e
Hannes Harnisch <h.harnisch@fu-berlin.de>
2024-05-31 22:54:39 +0200

    feat: maxflow gurobi solver implementation

--- lib/solver/gurobiMaxFlowSolver.py
+++ lib/solver/gurobiMaxFlowSolver.py
@@ -1,10 +1,46 @@
+from lib.network.capacitatedArc import CapacitatedArc
 from lib.network.network import Network
 from lib.solver.solver import Solver
-from lib.solver.solverState import SolverState
+from lib.solver.solverState import SolverSolution, SolverState
 import gurobipy as gp
 
 
 class GurobiMaxFlowSolver(Solver):
     def solve(self, state: SolverState) -> SolverState:
-        # TODO: implement Max Flow Solver
-        return state
+
+        network = state.network
+        source = network.get_node("s")
+        sink = network.get_node("t")
+
+        # Create a new model
+        max_flow = gp.Model("max_flow")
+
+        # Create variables
+        variables: dict[CapacitatedArc, gp.Var] = {}
+        for arc in network.arcs:
+            variables[(arc.from_node, arc.to_node)] = max_flow.addVar(lb=0, ub=arc.capacity.ub, name=f"x_{arc.from_node}_{arc.to_node}")
+
+        # Objective function
+        max_flow.setObjective(gp.quicksum(variables[(arc.from_node, arc.to_node)] for arc in network.arcs if arc.from_node == source.id), gp.GRB.MAXIMIZE)
+
+        # Flow conservation constraints
+        for node in network.nodes:
+            if node == source or node == sink:
+                continue
+            inflow = gp.quicksum(variables[(arc.from_node,arc.to_node)] for arc in network.arcs if arc.to_node == node.id)
+            outflow = gp.quicksum(variables[(arc.from_node,arc.to_node)] for arc in network.arcs if arc.from_node == node.id)
+            max_flow.addConstr(inflow - outflow == 0, name=f"flow_maintenance_{node.id}")
+
+        # Solve the model
+        max_flow.optimize()
+
+        if max_flow.status != gp.GRB.OPTIMAL:
+            return None
+
+        return SolverState(
+            network = state.network,
+            solution = SolverSolution(
+                flow={arc: modelVar.x for arc, modelVar in variables.items()}, 
+                target_value=max_flow.ObjVal
+            )
+        )
